21a7364484fecbb2b2da70cbd5109f4d52c20b21
Drew Ross <mail.drew.ross@gmail.com>
2020-09-08 13:05:52 -0700

    Add logic for move_to_fron and move_to_end methods. Test OK.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -98,14 +98,34 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if self.head is not node:
+            node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev
+            old_head = self.head
+            self.head = node
+            node.next = old_head
+            node.prev = None
+            old_head.prev = self.head
+            if self.head is self.tail:
+                self.tail = old_head
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if self.tail is not node:
+            node.next.prev = node.prev
+            if node.prev:
+                node.prev.next = node.next
+            old_tail = self.tail
+            self.tail = node
+            node.prev = old_tail
+            node.next = None
+            old_tail.next = self.tail
+            if self.head is self.tail:
+                self.head = old_tail
 
     """
     Deletes the input node from the List, preserving the 
