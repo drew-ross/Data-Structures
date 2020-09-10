9a0cecb314766b354f79268133adfca4b9532452
Drew Ross <mail.drew.ross@gmail.com>
2020-09-10 12:27:08 -0700

    Add logic for insert, _bubble_up, __str__, and get index helper methods.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,9 +1,16 @@
+import math
+
+
 class Heap:
     def __init__(self):
         self.storage = []
 
+    def __str__(self):
+        return f"{self.storage}"
+
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
         pass
@@ -15,7 +22,28 @@ class Heap:
         pass
 
     def _bubble_up(self, index):
-        pass
+        node = self.storage[index]
+        parent = self.storage[(self._get_parent_index(index))]
+        while node > parent:
+            self.storage[index] = parent
+            self.storage[(self._get_parent_index(index))] = node
+            index = self._get_parent_index(index)
+            if index < 0:
+                break
+            node = self.storage[index]
+            parent = self.storage[(self._get_parent_index(index))]
 
     def _sift_down(self, index):
         pass
+
+    def _get_parent_index(self, index):
+        parent_index = math.floor((index - 1) / 2)
+        if parent_index < 0:
+            parent_index = 0
+        return parent_index
+
+    def _get_left_index(self, index):
+        return 2 * index + 1
+
+    def _get_right_index(self, index):
+        return 2 * index + 2
